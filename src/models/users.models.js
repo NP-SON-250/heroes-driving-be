49a99472c8bb09591cdde0cb7f322cba8fcca026
NP-SON <hakizimanaalexis123@gmail.com>
2024-06-09 23:47:24 +0300

    Add user model

---
+++ src/models/users.models.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { v4 as uuidv4 } from 'uuid';
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, default: uuidv4 },
+    fullname: { type: String },
+    username: { type: String },
+    password: { type: String },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    createdAt: { type: Date, default: Date.now }
+  }
+);
+
+const userModel = mongoose.model("users", userSchema);
+export default userModel;
